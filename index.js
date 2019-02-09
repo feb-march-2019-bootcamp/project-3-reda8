c1890e57750715c51c2cca2778a3e16f7cb939f3
reda8 <39913968+reda8@users.noreply.github.com>
2019-02-09 04:05:22 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function palindrome (str){
 
+    let smallStr = str.toLowerCase(str);
+    let replacedStr = smallStr.replace(/\W|\_|\*/gi, "");
+    let reversedStr = replacedStr.split("").reverse().join("");
+    if (replacedStr === reversedStr){
+    
+        return true;
+    }else{
+        return false;
+    }
+    }
+    
+    
+    console.log(palindrome("EyE"));
